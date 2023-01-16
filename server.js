1357d7a97fa580f8bd38cb0f0a02b09ce2066901
laos0 <isonylao@gmail.com>
2023-01-16 13:56:50 -0600

    Provide cors, able to retrieve message from angular

--- server.js
+++ server.js
@@ -2,13 +2,34 @@ const express = require('express')
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: ['http://localhost:4200'],
+    },
+});
 const port = 8080;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 })
 
-server.listen(port, () => {
-    console.log("Server is running on *port: " + port);
-});
+io.on("connection", (socket) => {
+    console.log('a user connected');
+
+    // we have to put this here to ensure that we are connected before sending out
+    // messages
+    socket.on('message', (mgs) => {
+        console.log('message: ' + mgs);
+        socket.emit('serverMessage', "Hello from server!");
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+
+})
+
+io.listen(port);
+// server.listen(port, () => {
+//     console.log("Server is running on *port: " + port);
+// });
